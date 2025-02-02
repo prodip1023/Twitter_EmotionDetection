2987933f623ccc5dbb3aff31a9bca97d1d37e6ea
“prodip1023” <prodip1023@gmail.com>
2025-02-02 22:29:08 +0530

    add file

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # Twitter_EmotionDetection
+
+```
+
+git init
+
+dvc init
+
+dvc repro
+
+dvc dag
+
+dvc metrics show
+```
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "MlPipeline",
+    version= "0.0.0",
+    author="Prodip",
+    author_email="prodip1023@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
